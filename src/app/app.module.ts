b35c1bb9d287710847b46e14b18cac7d0281ba61
Jade <jade.cmail@gmail.com>
2021-11-07 18:53:12 +0100

    started the service2 quest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cocktail-service.service.ts
+++ src/app/cocktail-service.service.ts
@@ -1,32 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Cocktail } from './shared/cocktail.model';
+import { HttpClient } from  '@angular/common/http';
+import { Observable } from  'rxjs';
+import { map } from  "rxjs/operators";
 
 @Injectable({
   providedIn: 'root',
 })
 export class CocktailServiceService {
   cocktails: Cocktail[];
-  constructor() {
-    this.cocktails = [
-      new Cocktail(
-        'BloodyMary',
-        5,
-        'https://www.bar-maison.com/wp-content/uploads/2021/01/bloody-mary.jpg'
-      ),
-      new Cocktail(
-        'Margarita',
-        3,
-        'https://www.bar-maison.com/wp-content/uploads/2021/01/cocktail-margarita.jpg'
-      ),
-      new Cocktail(
-        'Manhattan',
-        4,
-        'https://www.liquor.com/thmb/zJ7_To3UNS5DNJ0aeyc4dVHUAac=/735x0/__opt__aboutcom__coeus__resources__content_migration__liquor__2018__05__10144903__Manhattan-720x720-recipe-9497922907c14d91898f557cb51f2ea3.jpg'
-      ),
-    ];
+  private service : HttpClient; 
+  constructor(param_service: HttpClient) {
+  this.service = param_service;
   }
 
-  getCocktails(): Cocktail[] {
-    return this.cocktails;
+  getCocktails(): Observable<string> {
+    const obs1 : Observable<any> = this.service.get("assets/cocktails.json")
   }
 }
